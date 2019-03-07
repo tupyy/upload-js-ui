2725c9fae004132601d4163d7c80f7d7f479fcbd
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2019-03-07 16:24:25 +0100

    first draft of the fileupload ui

---
+++ js/upload/file-entry-ui.js
@@ -0,0 +1,102 @@
+$(function () {
+    $.widget("custom_ui.fileui", {
+        options: {
+            filename: "file",
+            id: 0,
+            progressValue: 0
+        },
+
+        _create: function () {
+            this.options.value = this._constrain(this.options.progressValue);
+            this.element.addClass("upload-file");
+            this.mainElement = $("<td></td>").appendTo(this.element);
+
+            this.label = $("<span></span>", {
+                text: this.filename,
+                "class": "file-label"
+            }).appendTo(this.mainElement);
+
+            this.progressbarContainer = $("<div></div>", {
+                "class": "progress"
+            }).appendTo(this.mainElement);
+
+            this.progressbar = $("<div></div>", {
+                "class": "progress-bar bg-info",
+                "role": "progress-bar",
+                "aria-valuemin": "0",
+                "aria-valuenow": "25",
+                "aria-valuemax": "100",
+            }).appendTo(this.progressbarContainer);
+
+            this.uploadButton = $("<button>", {
+                text: "Upload",
+                "class": "ui-upload-button btn btn-warning"
+            }).appendTo(this.mainElement)
+                .button();
+
+            this.deleteButton = $("<button>", {
+                text: "Delete",
+                "class": "ui-upload-button btn btn-danger"
+            }).appendTo(this.mainElement)
+                .button();
+
+            this._on(this.deleteButton, {
+                click: "_delete"
+            });
+        },
+
+        refresh: function () {
+            this.label.text(this.options.filename + " " + this.options.id);
+            this.progressbar.css("width", this.options.progressValue + "%");
+            if ( this.options.value === 100 ) {
+                this._trigger( "complete", null, { value: 100 } );
+            }
+        },
+
+        _setOption: function (key, value) {
+            if (key === "value") {
+                value = this._constrain(value);
+            }
+            this._super(key, value);
+            this.refresh();
+        },
+
+        _setOptions: function (options) {
+            this._super(options);
+            this.refresh();
+        },
+
+        _delete: function () {
+            this._trigger("delete", event, {id: this.options.id});
+        },
+
+        _constrain: function( value ) {
+            if ( value > 100 ) {
+                value = 100;
+            }
+            if ( value < 0 ) {
+                value = 0;
+            }
+            return value;
+        }
+    });
+
+    let elem = $(".file-list-container");
+    uiFiles = [];
+    for (let i = 0; i < 4; i++) {
+        var b = $("<tr class=\"list-group-item\"></tr>")
+            .appendTo(elem)
+            .fileui().bind("fileuidelete", function (event, data) {
+                $.each(uiFiles, function (index, value) {
+                    if (value.fileui('option', 'id') === data.id) {
+                        uiFiles.splice(index, 1);
+                        value.remove();
+                        return false;
+                    }
+                });
+            });
+        b.fileui("option", 'id', i);
+        b.fileui("option", "progressValue", "45");
+        uiFiles.push(b);
+    }
+});
