fe80b80303147aa6a197c74fd4d7329652259abf
cosmin <cosmin@home.net>
2019-03-12 20:10:43 +0100

    bind mainForm widget to #mainForm

--- static/js/upload/main.js
+++ static/js/upload/main.js
@@ -1,19 +1,3 @@
 $(document).ready(function() {
-    let elem = $(".file-list-container");
-    uiFiles = [];
-    for (let i = 0; i < 4; i++) {
-        var b = $("<div class=\"list-group-item\"></div>")
-            .appendTo(elem)
-            .fileui().bind("fileuidelete", function (event, data) {
-                $.each(uiFiles, function (index, value) {
-                    if (value.fileui('option', 'id') === data.id) {
-                        uiFiles.splice(index, 1);
-                        value.remove();
-                        return false;
-                    }
-                });
-            });
-        b.fileui("option", 'id', i);
-        uiFiles.push(b);
-    }
+   $("#mainForm").mainForm();
 });
